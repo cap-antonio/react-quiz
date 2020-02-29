13f6bb4c30bc2b445538bf64612edac43c998a73
cap.Antonio <cheremnykh.anton@gmail.com>
2020-02-29 17:13:30 +0100

    Updates

---
+++ precache-manifest.ca3b0c94b9a61af19055f6e67313c068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680e694889bb5d6dc0f25a1e27ed7b17",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "3fa6ad932c891a2291ef",
+    "url": "/react-quiz/static/css/main.a6f46adb.chunk.css"
+  },
+  {
+    "revision": "5ece575865911d8d16be",
+    "url": "/react-quiz/static/js/2.bcf2e4a2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-quiz/static/js/2.bcf2e4a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fa6ad932c891a2291ef",
+    "url": "/react-quiz/static/js/main.941fc50c.chunk.js"
+  },
+  {
+    "revision": "7f775d81005cc2bd6c56",
+    "url": "/react-quiz/static/js/runtime-main.fbee9deb.js"
+  }
+]);
